2c91d6398bc51e745a88b653f30fedf8e795e32b
Ramakrishna G. Kannari <rkannari@BLR.ALLEGISINDIA.COM>
2023-04-30 18:02:57 +0530

    chalk

---
+++ demo5.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+console.log("success");
+console.log(chalk.bold.red("error"));
+console.log(chalk.green("success"));
+console.log(chalk.green.bold("success"));
+console.log(chalk.green.inverse.bold("success"));
+console.log(chalk.yellow("warning"));
